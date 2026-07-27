e61b5dc2bad3aa9638137258ffae84386bd5432f
Mitch M <mitchm.imaging@me.com>
2026-07-27 11:16:27 -0700

    Create Song.cpp

---
+++ Song.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Song.h"    // Song.h has been included
+using namespace std;
+
+// Write the class definition for Song in THE OTHER FILE Song.h (see tabs at the top)
+
+// Write all of the methods for the Song class here.
+
